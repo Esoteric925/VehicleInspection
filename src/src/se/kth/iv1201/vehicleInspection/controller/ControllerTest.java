8556af5640c15a2435232a7eb2e7f4715ab00d4b
Araxi Mekhitarian <araxii@live.se>
2017-05-02 17:18:59 +0200

    creating unit tests

--- src/src/se/kth/iv1201/vehicleInspection/controller/ControllerTest.java
+++ src/src/se/kth/iv1201/vehicleInspection/controller/ControllerTest.java
@@ -3,58 +3,80 @@ package se.kth.iv1201.vehicleInspection.controller;
 import org.junit.Before;
 import org.junit.Test;
 import se.kth.iv1201.vehicleInspection.integration.Garage;
+import se.kth.iv1201.vehicleInspection.integration.InspectionRegistry;
+import se.kth.iv1201.vehicleInspection.integration.PaymentAuthorization;
+import se.kth.iv1201.vehicleInspection.integration.Printer;
+import se.kth.iv1201.vehicleInspection.model.CashRegister;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by araxi on 2017-05-01.
+ * Created by araxi on 2017-05-02.
  */
 public class ControllerTest {
     Garage garage;
+    Printer printer;
+    CashRegister cashRegister;
+    InspectionRegistry inspectionRegistry;
+    PaymentAuthorization paymentAuthorization;
+    Controller controller;
     @Before
     public void setUp() throws Exception {
 
         garage = new Garage();
+        printer = new Printer();
+        cashRegister = cashRegister;
+        inspectionRegistry = inspectionRegistry;
+        paymentAuthorization = paymentAuthorization;
+        controller = new Controller(garage, printer, cashRegister, inspectionRegistry, paymentAuthorization);
 
     }
 
     @Test
-    public void initiateInspection() throws Exception {
+    public void createInspection() throws Exception {
+
+    }
+
+    @Test
+    public void getInspectionList() throws Exception {
+    }
+
+    @Test
+    public void openDoor() throws Exception {
+
+        assertFalse(controller.closeTheDoor().equalsIgnoreCase("Garage is opening the door"));
+    }
+
+    @Test
+    public void nextCustomer() throws Exception {
     }
 
     @Test
     public void closeTheDoor() throws Exception {
-
     }
 
     @Test
     public void checkRegNr() throws Exception {
-
     }
 
     @Test
     public void checkWhatToInspect() throws Exception {
-
     }
 
     @Test
     public void storeItemResult() throws Exception {
-
     }
 
     @Test
     public void payByCash() throws Exception {
-
     }
 
     @Test
     public void payByCard() throws Exception {
-
     }
 
     @Test
     public void printInspectionResult() throws Exception {
-
     }
 
 }
